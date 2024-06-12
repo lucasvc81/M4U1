051bb62a3aee652362ff99ce2e3b7fefb5eea8b1
Lucas VC <lucasvc@hotmail.com>
2024-06-12 11:35:18 -0300

    nav

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = (props) => {
     return (
         <nav>
             <div>
-              <ul>
+              <ul className='holder'>
                 <li>< NavLink to="/" className={({ isActive }) => isActive ? "activo" : undefined }>Home</ NavLink></li>
                 <li><NavLink to="/nosotros" className={({ isActive }) => isActive ? "activo" : undefined }>Nosotros</NavLink></li>
                 <li>< NavLink to="/servicios" className={({ isActive }) => isActive ? "activo" : undefined }>Servicios</ NavLink></li>
